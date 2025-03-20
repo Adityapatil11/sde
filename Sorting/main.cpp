d4d2049481174437c26f84e958c023a662041c87
Aditya Patil <adityappatil11@gmail.com>
2025-03-20 23:36:04 +0530

    added debug support for windows

--- Sorting/main.cpp
+++ Sorting/main.cpp
@@ -5,14 +5,14 @@
 using namespace std;
 int main()
 {
-    int n;
-    cout<<"Enter the size of arr: ";
-    cin>>n;
-    int arr[100];
-    cout<<"Enter the arr: ";
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
+    int n =10;
+    // cout<<"Enter the size of arr: ";
+    // cin>>n;
+    int arr[10] = {45,7,1,9,2,3,6,8,4,5};
+    // cout<<"Enter the arr: ";
+    // for(int i=0;i<n;i++){
+    //     cin>>arr[i];
+    // }
     
     bubblesort_optimized(arr,n);
     // bubblesort(arr,n);
