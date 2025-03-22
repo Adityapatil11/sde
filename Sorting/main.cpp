7ae1d9d74013fd8a7745420fe1047bbbf6b63cac
Adityapatil11 <adityappatil11@gmail.com>
2025-03-22 19:28:14 +0530

    merge sort

--- Sorting/main.cpp
+++ Sorting/main.cpp
@@ -1,26 +1,20 @@
 #include<iostream>
 #include "selectionsort.h"
 #include "bubblesort.h"
+#include "mergesort.h"
 
 using namespace std;
 int main()
 {
     int n =10;
-    // cout<<"Enter the size of arr: ";
-    // cin>>n;
     int arr[10] = {45,7,1,9,2,3,6,8,4,5};
-    // cout<<"Enter the arr: ";
-    // for(int i=0;i<n;i++){
-    //     cin>>arr[i];
-    // }
-    
-    bubblesort_optimized(arr,n);
-    // bubblesort(arr,n);
-    //selectionsort(arr,n);
-    
+    vector<int> vec = {10,9,8,7,6,5,4,3,2,1};
+
+    mergesort(vec,0,n-1);
+
     cout<<"Sorted arr: ";
     for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+        cout<<vec[i]<<" ";
     }
     cout<<"\n";
     return 0;
